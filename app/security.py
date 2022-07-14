88aeb23e9c6a27208cd47e8484866524b4e48389
Denys G. Santos <gsdenys@gmail.com>
2022-07-14 08:21:38 -0400

    organize api token using env var (for production)

--- app/security.py
+++ app/security.py
@@ -6,13 +6,14 @@ from starlette.status import HTTP_403_FORBIDDEN
 
 
 API_KEY_NAME = "access_token"
-API_KEY =  os.environ['API_KEY']
+API_KEY = os.environ.get('API_KEY')
 
 # Development and Test environment
 if not API_KEY:
     API_KEY = "1234567890qwertyuiop"
 
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+
 
 def get_api_key(api_key_header: str = Security(api_key_header)):
     if api_key_header == API_KEY:
