5c40e852821557a9ec8fb1cfb95bd9a47c5773e0
Denys G. Santos <gsdenys@gmail.com>
2022-07-15 13:45:05 -0400

    pydoc to APIs

--- app/api/author.py
+++ app/api/author.py
@@ -17,6 +17,18 @@ router = APIRouter(prefix='/author')
 
 @router.get('/{id}', response_model=ResponseAuthor, status_code=200, tags=['Author'])
 async def get_author_by_id(id: str, api_key: APIKey = Depends(get_api_key)):
+    """Get an author based in their ID
+
+    Args:
+        id (str): the author ID
+        api_key (APIKey, optional): the header API Key. Defaults to Depends(get_api_key).
+
+    Raises:
+        HTTPException: 404 - author not found
+
+    Returns:
+        Author: the requested author
+    """
     author = db.session.query(ModelAuthor).filter(ModelAuthor.id == id).first()
 
     if author is None:
@@ -27,12 +39,32 @@ async def get_author_by_id(id: str, api_key: APIKey = Depends(get_api_key)):
 
 @router.get('/', response_model=list[ResponseAuthor], status_code=200, tags=['Author'])
 async def author(api_key: APIKey = Depends(get_api_key)):
+    """List all Authors from database
+
+    Args:
+        api_key (APIKey, optional): the header API Key. Defaults to Depends(get_api_key).
+
+    Returns:
+        List[Author]: A list of authors
+    """
     author = db.session.query(ModelAuthor).all()
     return author
 
 
 @router.post('/', response_model=ResponseAuthor, status_code=201, tags=['Author'])
 async def create_author(author: RequestAuthor, api_key: APIKey = Depends(get_api_key)):
+    """Create new Author
+
+    Args:
+        author (RequestAuthor): The author informations
+        api_key (APIKey, optional): the header API Key. Defaults to Depends(get_api_key).
+
+    Raises:
+        HTTPException: 500 - author not created
+
+    Returns:
+        Author: The created author
+    """
     db_author = ModelAuthor(name=author.name, email=author.email)
 
     try:
@@ -46,6 +78,16 @@ async def create_author(author: RequestAuthor, api_key: APIKey = Depends(get_api
 
 @router.put('/{id}', response_model=ResponseAuthor, status_code=200, tags=['Author'])
 async def update_author(id: str, author: RequestAuthor, api_key: APIKey = Depends(get_api_key)):
+    """Updates the author
+
+    Args:
+        id (str): the author ID
+        author (RequestAuthor): author's new content
+        api_key (APIKey, optional): the header API Key. Defaults to Depends(get_api_key).
+
+    Returns:
+        Author: the updated author
+    """
     db_author = db.session.query(ModelAuthor).filter(ModelAuthor.id == id).first()
 
     db_author.name = author.name
@@ -59,6 +101,12 @@ async def update_author(id: str, author: RequestAuthor, api_key: APIKey = Depend
 
 @router.delete('/{id}', status_code=204, tags=['Author'])
 async def delete_author(id: str, api_key: APIKey = Depends(get_api_key)):
+    """Delete author based in their ID
+
+    Args:
+        id (str): the author ID
+        api_key (APIKey, optional): the header API Key. Defaults to Depends(get_api_key).
+    """
     db_author = db.session.query(ModelAuthor).filter(ModelAuthor.id == id).first()
 
     db.session.delete(db_author)
--- app/api/entry.py
+++ app/api/entry.py
@@ -17,6 +17,18 @@ router = APIRouter(prefix='/entry')
 
 @router.get('/{id}', response_model=ResponseEntry, status_code=200, tags=['Entry'])
 async def get_entry_by_id(id: str, api_key: APIKey = Depends(get_api_key)):
+    """Get entry based in theri id
+
+    Args:
+        id (str): the entry id
+        api_key (APIKey, optional): the header API Key. Defaults to Depends(get_api_key).
+
+    Raises:
+        HTTPException: 404 - Entry not found
+
+    Returns:
+        Entry: The requested entry
+    """
     entry = db.session.query(ModelEntry).filter(ModelEntry.id == id).first()
 
     if entry is None:
@@ -27,12 +39,32 @@ async def get_entry_by_id(id: str, api_key: APIKey = Depends(get_api_key)):
 
 @router.get('/', response_model=list[ResponseEntry], status_code=200, tags=['Entry'])
 async def list_entries(api_key: APIKey = Depends(get_api_key)):
+    """List all entries from database
+
+    Args:
+        api_key (APIKey, optional): _description_. Defaults to Depends(get_api_key).
+
+    Returns:
+        list[Entry]: the entry list
+    """
     entry = db.session.query(ModelEntry).all()
     return entry
 
 
 @router.post('/', response_model=ResponseEntry, status_code=201, tags=['Entry'])
 async def create_entry(entry: RequestEntry, api_key: APIKey = Depends(get_api_key)):
+    """Create new Entry
+
+    Args:
+        entry (RequestEntry): The new entry json information
+        api_key (APIKey, optional): the header API Key. Defaults to Depends(get_api_key).
+
+    Raises:
+        HTTPException: 500 - cannot create entry
+
+    Returns:
+        Entry: The created entry
+    """
     db_entry = ModelEntry(
         description=entry.description,
         incomme=entry.incomme,
@@ -54,6 +86,16 @@ async def create_entry(entry: RequestEntry, api_key: APIKey = Depends(get_api_ke
 
 @router.put('/{id}', response_model=ResponseEntry, status_code=200, tags=['Entry'])
 async def update_entry(id: str, entry: RequestEntry, api_key: APIKey = Depends(get_api_key)):
+    """Update entry
+
+    Args:
+        id (str): the entry id
+        entry (RequestEntry): the entry new information
+        api_key (APIKey, optional): the header API Key. Defaults to Depends(get_api_key).
+
+    Returns:
+        Entry: the updated entry
+    """
     db_entry = db.session.query(ModelEntry).filter(ModelEntry.id == id).first()
 
     db_entry.description = entry.description
@@ -69,6 +111,12 @@ async def update_entry(id: str, entry: RequestEntry, api_key: APIKey = Depends(g
 
 @router.delete('/{id}', status_code=204, tags=['Entry'])
 async def delete_entry(id: str, api_key: APIKey = Depends(get_api_key)):
+    """Delete the entry based in their ID
+
+    Args:
+        id (str): the entry id
+        api_key (APIKey, optional): the header API Key. Defaults to Depends(get_api_key).
+    """
     db_entry = db.session.query(ModelEntry).filter(ModelEntry.id == id).first()
 
     db.session.delete(db_entry)
